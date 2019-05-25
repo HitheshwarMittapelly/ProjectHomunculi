d8816c9d26729fda41bb3b97feac4fde529a81f8
Hitesh Mittapelly <hitheshwar.m@gmail.com>
2019-05-24 20:15:13 -0600

    AI moves to player

---
+++ Assets/_Scripts/AIController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+	public static AIController instance;
+
+	private static int currentlyIdleAnimals = 0;
+	private static List<AnimalController> allAnimals = new List<AnimalController>();
+	// Start is called before the first frame update
+	void Start()
+    {
+		if (instance == null) {
+			instance = this;
+		}
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (allAnimals.Count > 0) {
+			FindDestinationForAnimals();
+		}
+    }
+
+
+	public static void AddAnimalToAI(GameObject i_NewAnimal) {
+		allAnimals.Add(i_NewAnimal.GetComponent<AnimalController>());
+	}
+
+	public static void FindDestinationForAnimals() {
+		foreach(var animal in allAnimals) {
+			if (animal.animalAIState == AnimalController.AIStates.Roaming || animal.animalAIState == AnimalController.AIStates.Idle) {
+				bool foundPlayer = animal.TryToFindPlayer();
+				if (foundPlayer) {
+					animal.MoveToPlayer();
+				}
+				else if (animal.animalAIState == AnimalController.AIStates.Idle) {
+					animal.MoveToRandomPoint();
+				}
+			}
+		}
+	}
+
+	public static void UpdateIdleAnimals() {
+		currentlyIdleAnimals++;
+		if(currentlyIdleAnimals >= 1) {
+			FindDestinationForAnimals();
+		}
+	}
+}
--- Assets/_Scripts/AnimalController.cs
+++ Assets/_Scripts/AnimalController.cs
@@ -1,30 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AnimalController : MonoBehaviour
 {
 	private CharacterController controller;
 	private Animator animatorController;
+	private Transform playerTransform;
+	private NavMeshAgent navMeshAgent;
+
+	public float repathDistance = 3.5f;
+	public int playerSearchRadius = 3;
+	public int randomSearchRadius = 10;
+	public LayerMask playerMask;
 	public float health;
 	public float speed;
+	[HideInInspector]
+	public AIStates animalAIState;
+	private Vector3 lastPos;
+
+	public enum AIStates { PursuingPlayer, ReadyToAttack,AttackingPlayer, Roaming, Idle};
     void Start()
     {
 		animatorController = GetComponent<Animator>();
-		controller = GetComponent<CharacterController>();
-
+		
+		navMeshAgent = GetComponent<NavMeshAgent>();
+		animalAIState = AIStates.Idle;
+		playerTransform = null;
+		AIController.AddAnimalToAI(gameObject);
+		AIController.FindDestinationForAnimals();
 	}
 
   
     void Update()
     {
-		Vector3 vel = transform.forward * speed * Time.deltaTime;
-		controller.SimpleMove(vel);
+		if (navMeshAgent) {
+			
+			if (animalAIState == AIStates.PursuingPlayer) {
+				if (!CheckIfReachedPlayer()) {
+					if (Vector3.Distance(playerTransform.position, lastPos) > repathDistance) {
+						lastPos = playerTransform.position;
+						navMeshAgent.destination = playerTransform.position;
+					};
+				}
+				
+			}
+			else if(animalAIState == AIStates.Roaming){
+				CheckIfReachedDestination();
+			}
+			animatorController.SetFloat("Blend", navMeshAgent.velocity.magnitude);
+		}
+		
+		//Vector3 vel = transform.forward * speed * Time.deltaTime;
+		//controller.SimpleMove(vel);
 		//transform.Translate(vel);
-		animatorController.SetFloat("Blend", vel.magnitude);
+		
+		
 
 	}
+	private void CheckIfReachedDestination() {
+		if (!navMeshAgent.pathPending) {
+			if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance) {
+				if (!navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude == 0f) {
+					
+					OnRoamingComplete();
+					
+				}
+			}
+		}
+	}
+	private bool CheckIfReachedPlayer() {
+		if (!navMeshAgent.pathPending) {
+			if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance) {
+				if (!navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude == 0f) {
 
+					OnReachingPlayer();
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 	public void TakeDamage(float damageAmount) {
 		health -= damageAmount;
 		if(health <= 0) {
@@ -37,5 +94,70 @@ public class AnimalController : MonoBehaviour
 		Destroy(gameObject);
 	}
 
+	public bool TryToFindPlayer() {
+		
 	
+		Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, playerSearchRadius,  playerMask);
+		if(hitColliders.Length > 0) {
+			playerTransform = hitColliders[0].transform;
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
+
+	public void MoveToPlayer() {
+		Debug.Log("Moving to Player");
+		navMeshAgent.SetDestination(playerTransform.position);
+		lastPos = playerTransform.position;
+		animalAIState = AIStates.PursuingPlayer;
+	}
+
+	public void MoveToRandomPoint() {
+		Debug.Log("Moving to Random Point");
+		playerTransform = null;
+		
+		navMeshAgent.SetDestination(RandomNavmeshLocation());
+		animalAIState = AIStates.Roaming;
+	}
+
+	public void OnReachingPlayer() {
+		playerTransform = null;
+		animalAIState = AIStates.ReadyToAttack;
+		
+		AttackPlayer();
+		Debug.Log("Reached player");
+		
+		
+	}
+
+	public void AttackPlayer() {
+		
+		animalAIState = AIStates.AttackingPlayer;
+	}
+	public void OnRoamingComplete() {
+		Debug.Log("Reached roam point");
+		animalAIState = AIStates.Idle;
+		playerTransform = null;
+		AIController.UpdateIdleAnimals();
+		
+	}
+
+	private Vector3 RandomNavmeshLocation() {
+		Vector3 randomDirection = Random.insideUnitSphere * randomSearchRadius;
+		randomDirection += transform.position;
+		NavMeshHit hit;
+		Vector3 finalPosition = Vector3.zero;
+		if (NavMesh.SamplePosition(randomDirection, out hit, randomSearchRadius, 1)) {
+			finalPosition = hit.position;
+		}
+		return finalPosition;
+	}
+
+
+	//public void AttackEndEvent() {
+
+	//}
 }
